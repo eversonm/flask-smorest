9dddb874c68427fba1389905ebc963e8552bb53e
Everson Magalhães <everson.m.cavalcante@gmail.com>
2022-10-27 00:06:27 -0300

    Schemas with Marshmallow

---
+++ schemas.py
@@ -0,0 +1,8 @@
+from marshmallow import Schema, fields
+
+
+class ItemSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+    store_id = fields.Str(required=True)
