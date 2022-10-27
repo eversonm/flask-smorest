9c6c98b00b1a56d7248a0884b6796ea1c10f654f
Everson Magalhães <everson.m.cavalcante@gmail.com>
2022-10-27 00:29:40 -0300

    Responses with flask-smorest

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,4 @@
 import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import items, stores
@@ -10,6 +9,7 @@ blp = Blueprint("Items", __name__, description="Operations on Items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -24,6 +24,7 @@ class Item(MethodView):
             abort(404, message="Item not found.")
 
     @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
         try:
             item = items[item_id]
@@ -36,10 +37,12 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
 
     @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
     def post(self, item_data):
         for item in items.values():
             if (
@@ -54,4 +57,4 @@ class ItemList(MethodView):
         item = {**item_data, "id": item_id}
         stores[item_id] = item
 
-        return item, 201
+        return item
--- resources/store.py
+++ resources/store.py
@@ -1,5 +1,4 @@
 import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
@@ -10,6 +9,7 @@ blp = Blueprint("Stores", __name__, description="Operations on Stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -23,12 +23,15 @@ class Store(MethodView):
         except KeyError:
             abort(404, message="store not found.")
 
+
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return stores.values()
 
     @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
     def post(self, store_data):
         for store in stores.values():
             if (store_data["name"] == store["name"]):
@@ -37,4 +40,4 @@ class StoreList(MethodView):
         store_id = uuid.uuid4().hex
         store = {**store_data, "id": store_id}
         stores[store_id] = store
-        return store, 201
+        return store
